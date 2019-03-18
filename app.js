6a2ea55cb0f124e7d832202857da1081131cc2f0
wongco <ginson@gmail.com>
2019-03-18 01:31:25 -0700

    add random content and scaffold answer

--- app.js
+++ app.js
@@ -1,5 +1,73 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const mainEl = document.querySelector('#main');
-  console.log('hello');
-  // Extension Time
+  // cache DOMLookups
+  const questionNode = document.querySelector('#question');
+  const categoryNode = document.querySelector('#category');
+  const answerNode = document.querySelector('#answer');
+
+  const prevButton = document.querySelector('#prev--button');
+  const nextButton = document.querySelector('#next--button');
+  const randomButton = document.querySelector('#random--button');
+  const answerButton = document.querySelector('#answer--button');
+
+  /** Test Question Data */
+  const TestQuestionsArray = [
+    {
+      id: 0,
+      category: 'HTML Foundation',
+      question: 'What is CSS?',
+      answer:
+        'A language used to modify the elements on a page with display attributes.'
+    },
+    {
+      id: 1,
+      category: 'HTML Foundation',
+      question: 'What is HTML?',
+      answer: 'A markup language for displaying information in the web browser.'
+    },
+    {
+      id: 2,
+      category: 'Framework',
+      question: 'What is React?',
+      answer:
+        'A popular web frontend framework for building web applications created by Facebook.'
+    }
+  ];
+
+  // preload first random Question
+  let currIdx = Math.floor(Math.random() * TestQuestionsArray.length);
+  let currentQuestion = TestQuestionsArray[currIdx];
+  updateQuestionToDom(currentQuestion);
+
+  /** Updates DOM with input question and answer */
+  function updateQuestionToDom({ question, answer, category }) {
+    questionNode.innerHTML = question;
+    answerNode.innerHTML = answer;
+    categoryNode.innerHTML = category;
+  }
+
+  // Event Handlers
+
+  // prevItem
+  prevButton.addEventListener('click', () => {
+    currIdx = currIdx === 0 ? TestQuestionsArray.length - 1 : currIdx - 1;
+    updateQuestionToDom(TestQuestionsArray[currIdx]);
+  });
+
+  // nextItem
+  nextButton.addEventListener('click', () => {
+    currIdx = currIdx === TestQuestionsArray.length - 1 ? 0 : currIdx + 1;
+    updateQuestionToDom(TestQuestionsArray[currIdx]);
+  });
+
+  // randomItem
+  randomButton.addEventListener('click', () => {
+    currIdx = Math.floor(Math.random() * TestQuestionsArray.length);
+    updateQuestionToDom(TestQuestionsArray[currIdx]);
+  });
+
+  // randomItem
+  answerButton.addEventListener('click', () => {
+    // show answer that was previously hidden
+    console.log('show answer!');
+  });
 });
